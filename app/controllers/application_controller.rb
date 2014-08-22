6e2afcdb3995e7f951fb17dbfc9dadf22b413ba8
Peggy Condon & Matt Williams <student@epicodus.com>
2014-08-22 16:42:11 -0700

    added list method to index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
 
   def index
+    @animals = Animal.all
+    @regions = Region.all
+    @sightings = Sighting.all
     render('/index')
   end
 
